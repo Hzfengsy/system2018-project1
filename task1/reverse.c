a89e5e70494fbcacb3fb2e0800df7458c641a93f
Hzfengsy <Hzfengsy@vip.qq.com>
2018-03-13 22:39:36 +0800

    finish task1

--- task1/reverse.c
+++ task1/reverse.c
@@ -4,13 +4,24 @@
 #include <unistd.h>
 #include <sys/reg.h>
 #include <sys/syscall.h>
+#include <string.h>
 
 const int long_size = sizeof(long);//字长 若本机是64位，则字长应为8字节long
+
+int min(int a, int b)
+{
+  return a < b ? a : b;
+}
 
 /**
 * TODO 反转str指针指向的字符串
 **/
 void reverse(char *str){
+  int len = strlen(str);
+  char *bak = (char*)malloc(len);
+  for (int i = 0; i < len; i++) bak[i] = str[i];
+  for (int i = 0; i < len; i++) str[i] = bak[len - i - 1];
+  free(bak);
 }
 
 /**
@@ -20,6 +31,16 @@ void reverse(char *str){
 * 你可能会用到：memcpy函数
 **/
 void getdata(pid_t child, long addr, char *str, int len){
+  long buf = 0;
+  int now = 0;
+  while (now < len)
+  {
+    buf = ptrace(PTRACE_PEEKDATA, child, addr, NULL);
+    // printf("%x\n", buf);
+    addr += 8;
+    for (int i = 0; i < 8 && buf; i++) str[now++] = buf & 0xff, buf >>= 8;
+  }
+  str[now] = 0;
 }
 
 /**
@@ -27,7 +48,17 @@ void getdata(pid_t child, long addr, char *str, int len){
  * 使用 ptrace 的 PTRACE_POKEDATA 来写，需要注意的是由于64位机器的字长是8byte。
  * */
 void putdata(pid_t child, long addr, char *str, int len){
-
+  long buf = 0;
+  int now = 0;
+  while (now < len)
+  {
+    buf = 0;
+    for (int i = min(7, len - now - 1); i >=0; i--) buf <<=8, buf |= str[now + i];
+    // printf("%x\n", buf);
+    ptrace(PTRACE_POKEDATA, child, addr, buf);
+    addr += 8;
+    now += 8;
+  }
 }
 
 int main(){
@@ -56,6 +87,9 @@ int main(){
       if(orig_rax == SYS_write) {
         if(toggle == 0) {
           toggle = 1;
+          params[0] = ptrace(PTRACE_PEEKUSER, child, 8 * RDI, NULL);
+          params[1] = ptrace(PTRACE_PEEKUSER, child, 8 * RSI, NULL);
+          params[2] = ptrace(PTRACE_PEEKUSER, child, 8 * RDX, NULL);
           /**
           * TODO 使用 PTRACE_PEEKUSER 来获取 RDI, RSI, RDX寄存器的值，
           * 分别存到params[0], params[1], params[2]中。
@@ -66,12 +100,14 @@ int main(){
           getdata(child, params[1], str,
                   params[2]);
           reverse(str);
+          // printf ("***%s***", str);
           putdata(child, params[1], str,
                   params[2]);
          } else {
           toggle = 0;
        }
      }
+     ptrace(PTRACE_SYSCALL, child, 0, 0);
     /**
     * TODO 使用 PTRACE_SYSCALL 来让子进程进行系统调用。
     **/
